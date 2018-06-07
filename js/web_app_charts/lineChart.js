46c66964c87ebbe43e4aef44ec6581fb9506fd6f
AthenaOzanich <athenaozanich@gmail.com>
2018-06-07 12:36:31 -0400

    Adds initial files
    Adds the index, and js files.

---
+++ js/web_app_charts/lineChart.js
@@ -0,0 +1,60 @@
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var config = {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: window.chartColors.red,
+      borderColor: window.chartColors.red,
+      data: [
+        500,
+        1000,
+        randomScalingFactor(),
+        0,
+        0,
+        0,
+        0
+      ],
+      fill: false,
+    }]
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Month'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Value'
+        }
+      }]
+    }
+  }
+};
+
+window.onload = function() {
+  var ctx = document.getElementById('lineChart').getContext('2d');
+  window.myLine = new Chart(ctx, config);
+};
+
+var colorNames = Object.keys(window.chartColors);
